92cf5b9bf59d0edbabb3192ec1217ff7cfaffa44
kajananan <nantheeswaran.20230944@iit.ac.lk>
2025-03-07 23:42:50 +0530

    Update PaymentPage.js
    in payment page for travel lotus i did some codings

--- src/Views/PaymentPage.js
+++ src/Views/PaymentPage.js
@@ -1,1 +1,13 @@
-//payment page for the travel lotus started
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { doc, updateDoc, collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
+
+const PaymentPage = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { roomId } = location.state;
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    console.log(user);
